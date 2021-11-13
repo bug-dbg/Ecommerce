fbffef8b92b7a9d597ef584706df586742989f84
Ren <renactub@gmail.com>
2021-11-13 19:21:20 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,36 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const fileUpload = require('express-fileupload')
+const cookieParser = require('cookie-parser')
+
+const app = express()
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors())
+app.use(fileUpload({
+    useTempFiles: true
+}))
+
+// Routes
+app.use('/user', require('./routes/userRouter'))
+app.use('/api', require('./routes/categoryRouter'))
+app.use('/api', require('./routes/upload'))
+app.use('/api', require('./routes/productRouter'))
+
+// Connect to mongodb
+mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to Mongodb'))
+
+
+app.get('/', (req, res) => {
+    res.json({msg: "Hello There"})
+})
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
